406de23aba2bfb95aace1f39adb04bd2d563fca5
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-09-22 00:06:26 +0200

    Add OLXLocalization class to return region_id based on city name (#9)

--- olx.py
+++ olx.py
@@ -13,3 +13,40 @@ class LocalizationData:
     region_id: Optional[int] = None
     city_id: Optional[int] = None
     city_name: Optional[str] = None
+
+
+class OLXLocalization:
+    """
+    Fetch localization data from OLX API
+    """
+
+    def __init__(self, city_name: str):
+        self.city_name = city_name
+        self.base_url = (
+            f"https://www.olx.pl/api/v1/friendly-links/query-params/{self.city_name}"
+        )
+
+    def get_localization_data(self) -> dict | None:
+        response = requests.get(self.base_url)
+        if response.status_code != 200:
+            return None
+        return json.loads(response.content)
+
+    def return_localization_data(self) -> LocalizationData | None:
+        """
+        Returning localization data
+        """
+        data = self.get_localization_data()
+        if data:
+            return LocalizationData(
+                data["data"]["region_id"],
+                data["data"]["city_id"],
+                data["metadata"]["names"]["location"]["city"]["name"],
+            )
+        return None
+
+
+if __name__ == "__main__":
+    l = OLXLocalization("Zdunska-wola")
+    x = l.return_localization_data()
+    print(x)
