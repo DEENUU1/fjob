e48375004b5b136ecc9366acf2fdbf71096ec4ea
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-09-22 21:35:09 +0200

    Add Salary and Offers models (#16)

--- fjob/fjob/settings.py
+++ fjob/fjob/settings.py
@@ -21,6 +21,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # project applications
+    "offers",
 ]
 
 MIDDLEWARE = [
