ccb855311cceb95f6dd7ac5886b0c2086f9c1f09
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-09-22 22:14:10 +0200

    Register models in admin dashboard

--- fjob/offers/admin.py
+++ fjob/offers/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
+from .models import Salary, Offers
 
-# Register your models here.
+
+@admin.register(Salary)
+class SalaryAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "salary_from",
+        "salary_to",
+        "currency",
+        "contract_type",
+        "work_schedule",
+    )
+    list_filter = ("currency", "contract_type", "work_schedule")
+    search_fields = ("currency", "contract_type", "work_schedule")
+
+
+@admin.register(Offers)
+class OffersAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "offer_id",
+        "url",
+        "street",
+        "region",
+        "remote",
+        "hybrid",
+        "date_created",
+        "date_finished",
+    )
+    list_filter = (
+        "remote",
+        "hybrid",
+        "experience_level",
+        "date_created",
+        "date_finished",
+    )
+    search_fields = (
+        "title",
+        "offer_id",
+        "region",
+        "experience_level",
+        "skills",
+        "company_name",
+    )
