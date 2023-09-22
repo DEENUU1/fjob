b456c4fd39eab1d9adbc5757064c166c6ee25ec4
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-09-22 22:28:55 +0200

    Feature: save_data method to save scraped and parsed data to database (#26)

--- fjob/scrapers/scraper.py
+++ fjob/scrapers/scraper.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 from typing import Dict, List, Optional
 from dataclasses import dataclass
 from ..offers.models import Offers, Salaries
+from django.db import transaction
 
 
 @dataclass
@@ -48,5 +49,52 @@ class Scraper(ABC):
     def parse_offer(self, json_data: List[Dict[str, str]]):
         pass
 
-    def save_data(self, data_list: List[ParsedOffer]) -> bool:
-        pass
+    @transaction.atomic()
+    def save_data(self, data_list: List[ParsedOffer]):
+        """
+        Save parsed data to database.
+
+        :param data_list: List of parsed data.
+        :return: List of saved data.
+        :rtype: List[Offers]
+
+        """
+
+        for parsed_offer in data_list:
+            try:
+                offer = Offers.objects.get(url=parsed_offer.url)
+            except Offers.DoesNotExist:
+                offer = Offers(url=parsed_offer.url)
+
+            offer.title = parsed_offer.title
+            offer.url = parsed_offer.url
+            offer.street = parsed_offer.street
+            offer.region = parsed_offer.region
+            offer.additional_data = parsed_offer.additional_data
+            offer.description = parsed_offer.description
+            offer.remote = parsed_offer.remote
+            offer.hybrid = parsed_offer.hybrid
+            offer.country = parsed_offer.country
+            offer.city = parsed_offer.city
+            offer.date_created = parsed_offer.date_created
+            offer.date_finished = parsed_offer.date_finished
+            offer.experience_level = parsed_offer.experience_level
+            offer.skills = parsed_offer.skills
+            offer.company_name = parsed_offer.company_name
+            offer.company_logo = parsed_offer.company_logo
+
+            offer.save()
+
+            if parsed_offer.salary:
+                for salary_data in parsed_offer.salary:
+                    salary = Salaries(
+                        salary_from=salary_data.salary_from,
+                        salary_to=salary_data.salary_to,
+                        currency=salary_data.currency,
+                        contract_type=salary_data.contract_type,
+                        work_schedule=salary_data.work_schedule,
+                    )
+                    salary.save()
+                    offer.salary.add(salary)
+
+            return offer
