b763ebf35de5619f1c3ed8067fdf28b5055cca2c
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-09-24 20:07:07 +0200

    Create dict with all availables urls (#6)

--- nofluffjobs.py
+++ nofluffjobs.py
@@ -12,10 +12,42 @@ class Data:
 
 
 page_number = 1
-
 session = HTMLSession()
-
 offers_ls = []
+
+
+urls = {
+    "backend": "https://nofluffjobs.com/pl/backend",
+    "frontend": "https://nofluffjobs.com/pl/frontend",
+    "fullstack": "https://nofluffjobs.com/pl/fullstack",
+    "mobile": "https://nofluffjobs.com/pl/mobile",
+    "embedded": "https://nofluffjobs.com/pl/embedded",
+    "ai": "https://nofluffjobs.com/pl/artificial-intelligence",
+    "data": "https://nofluffjobs.com/pl/data",
+    "business-intelligence": "https://nofluffjobs.com/pl/business-intelligence",
+    "business-analyst": "https://nofluffjobs.com/pl/business-analyst",
+    "product-management": "https://nofluffjobs.com/pl/product-management",
+    "testing": "https://nofluffjobs.com/pl/testing",
+    "devops": "https://nofluffjobs.com/pl/devops",
+    "sys-administrator": "https://nofluffjobs.com/pl/sys-administrator",
+    "security": "https://nofluffjobs.com/pl/security",
+    "architecture": "https://nofluffjobs.com/pl/architecture",
+    "game-dev": "https://nofluffjobs.com/pl/game-dev",
+    "project-manager": "https://nofluffjobs.com/pl/project-manager",
+    "agile": "https://nofluffjobs.com/pl/agile",
+    "design": "https://nofluffjobs.com/pl/design",
+    "support": "https://nofluffjobs.com/pl/support",
+    "erp": "https://nofluffjobs.com/pl/erp",
+    "other": "https://nofluffjobs.com/pl/other",
+    "hr": "https://nofluffjobs.com/pl/hr",
+    "marketing": "https://nofluffjobs.com/pl/marketing",
+    "sales": "https://nofluffjobs.com/pl/sales",
+    "finance": "https://nofluffjobs.com/pl/finance",
+    "office-administration": "https://nofluffjobs.com/pl/office-administration",
+    "consulting": "https://nofluffjobs.com/pl/consulting",
+    "customer-service": "https://nofluffjobs.com/pl/customer-service",
+}
+
 
 while True:
     url = f"https://nofluffjobs.com/pl/backend?page={page_number}"
