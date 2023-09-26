966ad29bc03c0b4da2e0f21d23c316915114ca32
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-09-26 23:26:36 +0200

    improve task to run dynamic scrapers (#51)

--- fjob/scrapers/tasks.py
+++ fjob/scrapers/tasks.py
@@ -3,10 +3,10 @@ from .pracujpl import run as run_pracujpl
 
 
 # Add celery later
-def task_run_olx(city: str) -> None:
-    run_olx(city)  # Add delay
+def task_run_olx(city: str, query: str = None) -> None:
+    run_olx(city=city, query=query)  # Add delay
 
 
 # Add celery later
-def task_run_pracujpl(query: str, region: str, city: str) -> None:
-    run_pracujpl(query, region, city)  # Add delay
+def task_run_pracujpl(city: str, query: str = None, region: str = None) -> None:
+    run_pracujpl(query=query, region=region, city=city)  # Add delay
