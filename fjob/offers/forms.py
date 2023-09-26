c3b221826a5c56503e724cb54eb1c17b13eab6e1
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-09-26 12:52:14 +0200

    Add chocice field to select Country in OfferFilterForm

--- fjob/offers/forms.py
+++ fjob/offers/forms.py
@@ -3,7 +3,13 @@ from django import forms
 
 class OfferFilterForm(forms.Form):
     query = forms.CharField(max_length=100, required=False)
-    country = forms.CharField(max_length=50, required=False)
+    # country = forms.CharField(max_length=50, required=False)
+    country = forms.ChoiceField(
+        choices=[
+            ("Poland", "Poland"),
+            ("Germany", "Germany"),
+        ]
+    )
     city = forms.CharField(max_length=50, required=False)
     min_salary = forms.IntegerField(required=False)
     max_salary = forms.IntegerField(required=False)
