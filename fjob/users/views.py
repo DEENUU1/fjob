7d670676db3bc4f34021b797256ebe6a7254724c
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-09-27 23:18:52 +0200

    implement register endpoint (#53)

--- fjob/users/views.py
+++ fjob/users/views.py
@@ -3,3 +3,20 @@ from rest_framework.authentication import SessionAuthentication
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import permissions, status
+from .validations import custom_validation, validate_password, validate_email
+from .serializers import UserSerializer, UserRegisterSerializer, UserLoginSerializer
+
+
+class UserRegisterView(APIView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request):
+        clean_data = custom_validation(request.data)
+        serializer = UserRegisterSerializer(data=clean_data)
+
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.create(clean_data)
+            if user:
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(status=status.HTTP_400_BAD_REQUEST)
