c93bbce4f99b217d81bd498f2d11bbc5d805bd11
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-09-27 23:05:15 +0200

    Implement CustomUserManager model to create normal user and superuser object (#52)

--- fjob/fjob/settings.py
+++ fjob/fjob/settings.py
@@ -13,6 +13,11 @@ DEBUG = True
 DATA_UPLOAD_MAX_NUMBER_FIELDS = 9999999
 ALLOWED_HOSTS = []
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+CORS_ALLOWED_CREDENTIALS = True
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -29,6 +34,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
--- fjob/users/models.py
+++ fjob/users/models.py
@@ -1,5 +1,41 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, first_name=None, last_name=None):
+        if not email:
+            raise ValueError("An email address is required")
+        if not password:
+            raise ValueError("A password is required")
+        if not first_name or not last_name:
+            raise ValueError("A first and last name are required")
+        user = self.model(
+            email=self.normalize_email(email),
+            first_name=first_name,
+            last_name=last_name,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, first_name=None, last_name=None):
+        if not email:
+            raise ValueError("An email address is required")
+        if not password:
+            raise ValueError("A password is required")
+        if not first_name or not last_name:
+            raise ValueError("A first and last name are required")
+        user = self.model(
+            email=self.normalize_email(email),
+            first_name=first_name,
+            last_name=last_name,
+        )
+        user.set_password(password)
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
 
 
 class CustomUser(AbstractUser):
