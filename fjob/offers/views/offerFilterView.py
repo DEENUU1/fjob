35bc3c7b97eeb73f7c706fd13326fd790bcd0fb8
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-09-28 20:51:20 +0200

    Update OffefilterView

--- fjob/offers/views/offerFilterView.py
+++ fjob/offers/views/offerFilterView.py
@@ -21,29 +21,30 @@ class OfferFilterView(ListAPIView):
         experience_level = self.request.query_params.get("experience_level")
         advanced = self.request.query_params.get("advanced")
 
+        if query:
+            queryset = queryset.filter(
+                Q(title__icontains=query) | Q(description__icontains=query)
+            )
+        if country:
+            queryset = queryset.filter(country__icontains=country)
+        if city:
+            queryset = queryset.filter(city__icontains=city)
+        if min_salary:
+            queryset = queryset.filter(salary__gte=min_salary)
+        if max_salary:
+            queryset = queryset.filter(salary__lte=max_salary)
+        if experience_level:
+            queryset = queryset.filter(experience_level=experience_level)
+
         # if the user does not use advanced search, he will only receive offers from the database
         if not advanced:
-            if query:
-                queryset = queryset.filter(
-                    Q(title__icontains=query) | Q(description__icontains=query)
-                )
-            if country:
-                queryset = queryset.filter(country__icontains=country)
-            if city:
-                queryset = queryset.filter(city__icontains=city)
-            if min_salary:
-                queryset = queryset.filter(salary__gte=min_salary)
-            if max_salary:
-                queryset = queryset.filter(salary__lte=max_salary)
-            if experience_level:
-                queryset = queryset.filter(experience_level=experience_level)
-
             return queryset
+
         # however, when using advanced search, it will receive offers from the database and scrapers will be launched
         else:
             # if country == "Poland":
             olx_data = olx.run(False, False, "Zduńska Wola")
             pracujpl_data = pracujpl.run(False, False, "Zduńska Wola")
-            queryset = list(queryset) + olx_data + pracujpl_data
+            advanced_queryset = list(queryset) + olx_data + pracujpl_data
 
-            return queryset
+            return advanced_queryset
