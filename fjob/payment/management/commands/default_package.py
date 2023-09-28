27756c57b5b66934d2183c5a9e201028d2f20b41
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-09-28 23:07:53 +0200

    Add command to create default Package objects (#64)

---
+++ fjob/payment/management/commands/default_package.py
@@ -0,0 +1,26 @@
+from django.core.management.base import BaseCommand
+from payment.models import Package
+
+
+class Command(BaseCommand):
+    help = "Create default Package objects"
+
+    def handle(self, *args, **kwargs):
+        package_1 = Package.objects.create(
+            name="Free",
+        )
+        package_2 = Package.objects.create(
+            name="Basic",
+            price=100,
+            is_free=False,
+        )
+        package_3 = Package.objects.create(
+            name="Advanced",
+            price=500,
+            has_signals=True,
+            is_free=False,
+        )
+
+        package_1.save()
+        package_2.save()
+        package_3.save()
