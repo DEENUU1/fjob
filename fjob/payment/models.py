27f4789ade2d9e21210d85b202c91c9dd1f232c6
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-09-28 22:58:23 +0200

    Add Package class and property method to convert cents to dolars (#64)

--- fjob/payment/models.py
+++ fjob/payment/models.py
@@ -5,6 +5,32 @@ from django.contrib.auth import get_user_model
 UserModel = get_user_model()
 
 
+class Package(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField(default=0)
+    has_signals = models.BooleanField(default=False)
+    num_of_signals = models.IntegerField(default=0)
+    is_free = models.BooleanField(default=True)
+    free_users = models.IntegerField(default=5)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    @property
+    def cents_to_dollar(self):
+        if self.price == 0:
+            return 0
+        else:
+            return self.price / 100
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name_plural = "Packages"
+        verbose_name = "Package"
+
+    def __str__(self):
+        return f"{self.name} - {self.price}$"
+
+
 class Payment(models.Model):
     user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
     active = models.BooleanField(default=False)
