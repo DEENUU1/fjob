1fbcf985725c6ab1599802af6cacc559742c82fb
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-09-28 22:09:15 +0200

    Add trial field to CustomUser model

--- fjob/payment/models.py
+++ fjob/payment/models.py
@@ -9,6 +9,5 @@ class Payment(models.Model):
     user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
     active = models.BooleanField(default=False)
     stripe_checkout_id = models.CharField(max_length=500)
-
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
