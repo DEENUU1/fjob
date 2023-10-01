0d4ea6f5a8da7c884df06859994ff1d3e0271da3
DEENUU1 <kacperwlodarczy@protonmail.com>
2023-10-02 01:53:35 +0200

    Add function to update free_uses for free package (#66)

--- fjob/utils/update_free_uses.py
+++ fjob/utils/update_free_uses.py
@@ -4,6 +4,7 @@ from payment.models import Package, UserPackage
 UserModel = get_user_model()
 
 
-# def update_free_uses(user):
-#     user_package = UserPackage.objects.filter(user=user, active=True).first()
-#     if user_package.package == 1:
+def update_free_uses(user):
+    user_package = UserPackage.objects.filter(user=user, active=True).first()
+    if user_package.package == 1:
+        user_package.free_uses -= 1
