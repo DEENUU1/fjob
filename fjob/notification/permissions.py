be2cf367cf474477a329e32e8443ce88fd2270ed
DEENUU1 <kacperwlodarczy@protonmail.com>
2023-10-02 15:46:06 +0200

    update Notification permissions (#77)

--- fjob/notification/permissions.py
+++ fjob/notification/permissions.py
@@ -1,5 +1,6 @@
 from rest_framework import permissions
 from payment.models import UserPackage
+from .models import Notification
 
 
 class CanAccessNotification(permissions.BasePermission):
@@ -7,8 +8,15 @@ class CanAccessNotification(permissions.BasePermission):
         user = request.user
         if user.is_authenticated:
             try:
-                user_package = UserPackage.objects.get(user=user, active=True).first()
-                return user_package.id in [2, 3]
+                user_package = UserPackage.objects.filter(
+                    user=user, active=True
+                ).first()
+                if user_package.id in [2, 3]:
+                    count_notifications = Notification.objects.filter(user=user).count()
+                    if user_package.id == 2:
+                        return count_notifications <= 3
+                    if user_package.id == 3:
+                        return count_notifications <= 5
             except UserPackage.DoesNotExist:
                 return False
         return False
