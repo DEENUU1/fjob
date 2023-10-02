1934b1d94eb5fa5c1833b20602a7c7917120fbb7
DEENUU1 <kacperwlodarczy@protonmail.com>
2023-10-02 23:58:30 +0200

    Add permission class for GetUserPackage endpoint (#80)

--- fjob/payment/views/GetUserPackage.py
+++ fjob/payment/views/GetUserPackage.py
@@ -1,5 +1,6 @@
 from rest_framework import status
 from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -10,18 +11,23 @@ class GetUserPackage(APIView):
     authentication_classes = [
         SessionAuthentication,
     ]
+    permission_classes = [
+        IsAuthenticated,
+    ]
 
     def get(self, request):
-        user = self.request.user
+        user = request.user
         user_package = UserPackage.objects.filter(user=user, active=True).first()
 
         package = user_package.package if user_package else None
 
-        if not package:
+        if package:
+            return Response(
+                {"name": package.name, "price": package.price},
+                status=status.HTTP_200_OK,
+            )
+
+        else:
             return Response(
                 {"message": "No package found"}, status=status.HTTP_404_NOT_FOUND
             )
-
-        return Response(
-            {"name": package.name, "price": package.price}, status=status.HTTP_200_OK
-        )
