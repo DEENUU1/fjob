9ea43e86ec9d1656f17b1abefbfa3a5f92c3aea3
DEENUU1 <kacperwlodarczy@protonmail.com>
2023-10-02 20:52:39 +0200

    Add boolean field to Contact model - read (#80)

--- fjob/contact/views.py
+++ fjob/contact/views.py
@@ -12,5 +12,5 @@ class SendMessage(APIView):
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response({"message": "ok"}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
