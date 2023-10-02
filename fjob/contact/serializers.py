5e8a2f19cf46f9cb0aacd206af074b035fc4631b
DEENUU1 <kacperwlodarczy@protonmail.com>
2023-10-02 20:48:48 +0200

    Add email validation in contact form using regex (#80)

--- fjob/contact/serializers.py
+++ fjob/contact/serializers.py
@@ -1,22 +1,26 @@
 from .models import Contact
 
 from rest_framework import serializers
+import re
 
 
 class ContactSerializer(serializers.ModelSerializer):
     class Meta:
-        fields = '__all__'
+        fields = "__all__"
         model = Contact
 
     def validate(self, data):
-        if not data:
-            raise serializers.ValidationError("No data provided")
+        email = data.get("email")
+
         if not data.get("name"):
             raise serializers.ValidationError("No name provided")
-        if not data.get("email"):
+        if not email:
             raise serializers.ValidationError("No email provided")
         if not data.get("content"):
             raise serializers.ValidationError("No content provided")
+
+        if not re.match(r"^[\w\.-]+@[\w\.-]+$", email):
+            raise serializers.ValidationError("Invalid email address")
 
         return data
 
