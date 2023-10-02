cbc7bfc713d6f1e2e22e75671ce9f3b17073cb80
DEENUU1 <kacperwlodarczy@protonmail.com>
2023-10-02 15:29:19 +0200

    Add simple generics views to Create, delete and update notifications (#77)

--- fjob/notification/views.py
+++ fjob/notification/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Notification
+from .serializers import NotificationSerializer
+from rest_framework.authentication import SessionAuthentication
 
-# Create your views here.
+
+class NotificationCreateView(generics.CreateAPIView):
+    authentication_classes = (SessionAuthentication,)
+    queryset = Notification.objects.all()
+    serializer_class = NotificationSerializer
+
+
+class NotificationUpdateView(generics.UpdateAPIView):
+    authentication_classes = (SessionAuthentication,)
+    queryset = Notification.objects.all()
+    serializer_class = NotificationSerializer
+
+
+class NotificationDeleteView(generics.DestroyAPIView):
+    authentication_classes = (SessionAuthentication,)
+    queryset = Notification.objects.all()
+    serializer_class = NotificationSerializer
