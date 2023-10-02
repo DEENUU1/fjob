8ba66b706d6e9c5872c39ff437378e56ec8edf9d
DEENUU1 <kacperwlodarczy@protonmail.com>
2023-10-02 15:33:02 +0200

    Implement persmission classes for CRUD endpoints (#77) :

--- fjob/notification/views.py
+++ fjob/notification/views.py
@@ -1,22 +1,26 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Notification
 from .serializers import NotificationSerializer
 from rest_framework.authentication import SessionAuthentication
+from .permissions import CanAccessNotification
 
 
 class NotificationCreateView(generics.CreateAPIView):
     authentication_classes = (SessionAuthentication,)
+    permission_classes = [permissions.IsAuthenticated, CanAccessNotification]
     queryset = Notification.objects.all()
     serializer_class = NotificationSerializer
 
 
 class NotificationUpdateView(generics.UpdateAPIView):
     authentication_classes = (SessionAuthentication,)
+    permission_classes = [permissions.IsAuthenticated, CanAccessNotification]
     queryset = Notification.objects.all()
     serializer_class = NotificationSerializer
 
 
 class NotificationDeleteView(generics.DestroyAPIView):
     authentication_classes = (SessionAuthentication,)
+    permission_classes = [permissions.IsAuthenticated, CanAccessNotification]
     queryset = Notification.objects.all()
     serializer_class = NotificationSerializer
