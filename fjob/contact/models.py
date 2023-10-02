bd8ced8cabf72e293b759d1b1f4a4dfd7bfbabcd
DEENUU1 <kacperwlodarczy@protonmail.com>
2023-10-02 14:26:59 +0200

    Add Contact model (#76)

--- fjob/contact/models.py
+++ fjob/contact/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    content = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-date_created',)
+        verbose_name = 'Contact'
+        verbose_name_plural = 'Contacts'
+
+    def __str__(self):
+        return f"{self.name} - {self.email}"
