ccb27753632b8821b74d5a61df904f49e301dcf5
DEENUU1 <kacperwlodarczy@protonmail.com>
2023-10-08 17:17:02 +0200

    Add endpoint to check if user is Authenticated

---
+++ fjob/users/views/CheckAuthenticatedView.py
@@ -0,0 +1,25 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+
+class CheckAuthenticatedView(APIView):
+    def get(self):
+        user = self.request.user
+
+        try:
+            isAuthenticated = user.is_authenticated
+
+            if isAuthenticated:
+                return Response(
+                    {"isAuthenticated": "success"}, status=status.HTTP_200_OK
+                )
+            else:
+                return Response(
+                    {"isAuthenticated": "error"}, status=status.HTTP_403_FORBIDDEN
+                )
+        except:
+            return Response(
+                {"error": "Something went wrong when checking authentication status"},
+                status=status.HTTP_403_FORBIDDEN,
+            )
