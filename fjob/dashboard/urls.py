3f407f9cef2a43abfb81dd291b93da9635e4d6e2
DEENUU1 <kacperwlodarczy@protonmail.com>
2023-10-18 23:44:46 +0200

    Add UserStatsListView (#106)

--- fjob/dashboard/urls.py
+++ fjob/dashboard/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from .views import ReportListVIew
+from .views import ReportListVIew, UserStatsListView
 
 urlpatterns = [
     path("", ReportListVIew.ReportListView.as_view(), name="report_list"),
+    path(
+        "user_stats/", UserStatsListView.UserStatsListView.as_view(), name="user_stats"
+    ),
 ]
