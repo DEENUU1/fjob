aaec27a8062516e6906926315ab815868c48ee25
DEENUU1 <kacperwlodarczy@protonmail.com>
2023-10-26 01:02:02 +0200

    Add IsAuthenticated class to OfferListView

--- fjob/offers/views/OfferListView.py
+++ fjob/offers/views/OfferListView.py
@@ -1,5 +1,7 @@
 from rest_framework import generics
 from rest_framework.filters import OrderingFilter
+from rest_framework.permissions import IsAuthenticated
+
 from ..models import Offers
 from ..serializers.OfferSerializer import OffersSerializer
 from ..forms import OffersFilter
@@ -7,6 +9,9 @@ from django_filters import rest_framework as filters
 
 
 class OfferListView(generics.ListAPIView):
+    authentication_classes = [
+        IsAuthenticated,
+    ]
     queryset = Offers.objects.filter(is_active=True)
     serializer_class = OffersSerializer
     filter_backends = (filters.DjangoFilterBackend, OrderingFilter)
