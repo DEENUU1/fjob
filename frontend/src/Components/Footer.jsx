80849a941ef49210a0ddde250703dcb08acb6643
DEENUU1 <kacperwlodarczy@protonmail.com>
2023-10-27 19:13:17 +0200

    Add simple footer component

---
+++ frontend/src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+export const Footer = () => {
+    return (
+        <footer className="page-footer font-small blue footer">
+
+            <div className="footer-copyright text-center py-3">Made with by
+        <a href="https://about.me/adewaletoluwani" target="_blank" rel="noopener noreferrer" className="author"> Kacper Włodarczyk</a>
+            </div>
+
+        </footer>
+    );
+}
