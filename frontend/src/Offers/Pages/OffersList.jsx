fc41859f2d17b81248d661cdc6a708b284b7d2d4
DEENUU1 <kacperwlodarczy@protonmail.com>
2023-10-27 22:51:18 +0200

    Add sorting in OfferList page

--- frontend/src/Offers/Pages/OffersList.jsx
+++ frontend/src/Offers/Pages/OffersList.jsx
@@ -4,6 +4,14 @@ import axios from "axios";
 export const OfferList = () => {
   const [offers, setOffers] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [sortBy, setSortBy] = useState("-date_scraped");
+
+  const orderingOptions = {
+    "Newest": "-date_scraped",
+    "Oldest": "date_scraped",
+    "Highest Salary": "salary__salary_from",
+    "Lowest Salary": "-salary__salary_from",
+  };
 
   useEffect(() => {
     const token = localStorage.getItem("access_token");
@@ -13,6 +21,9 @@ export const OfferList = () => {
         headers: {
           Authorization: `Bearer ${token}`,
         },
+        params: {
+          ordering: sortBy,
+        },
       })
       .then((response) => {
         setOffers(response.data.results);
@@ -21,19 +32,32 @@ export const OfferList = () => {
       .catch((error) => {
         console.error(error);
       });
-  }, []);
+  }, [sortBy]);
+
+  const handleSortChange = (event) => {
+    const newSortBy = event.target.value;
+    setSortBy(newSortBy);
+  };
 
   if (isLoading) {
     return <div>Loading...</div>;
   }
 
   return (
-    <ul>
-      {offers.map((offer) => (
-        <li key={offer.id}>
-          {offer.title}
-        </li>
-      ))}
-    </ul>
+    <div>
+      <label htmlFor="sort">Sort by:</label>
+      <select id="sort" onChange={handleSortChange} value={sortBy}>
+        {Object.entries(orderingOptions).map(([label, value]) => (
+          <option key={value} value={value}>
+            {label}
+          </option>
+        ))}
+      </select>
+      <ul>
+        {offers.map((offer) => (
+          <li key={offer.id}>{offer.title}</li>
+        ))}
+      </ul>
+    </div>
   );
 };
