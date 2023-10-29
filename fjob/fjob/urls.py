84cd30766c6f0558b5dedb56d8aee32b90703c69
DEENUU1 <kacperwlodarczy@protonmail.com>
2023-10-29 16:20:35 +0100

    Fix url to use new View for token

--- fjob/fjob/urls.py
+++ fjob/fjob/urls.py
@@ -6,6 +6,7 @@ from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 from rest_framework_simplejwt import views as jwt_views
+from users.views.CustomTokenObtainPairView import CustomTokenObtainPairView
 
 
 schema_view = get_schema_view(
@@ -27,7 +28,7 @@ urlpatterns = [
     path("users/", include("users.urls")),
     path("messages/", include("contact.urls")),
     path("reports/", include("report.urls")),
-    path("token/", jwt_views.TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", jwt_views.TokenRefreshView.as_view(), name="token_refresh"),
     re_path(
         r"^swagger(?P<format>\.json|\.yaml)$",
