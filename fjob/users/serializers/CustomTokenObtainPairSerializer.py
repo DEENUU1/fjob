6e78f6962eabb7caf48a15a7a8302f8fcdbf2dea
DEENUU1 <kacperwlodarczy@protonmail.com>
2023-10-29 16:17:39 +0100

    Add custom jwt serializer to get user_id and username

---
+++ fjob/users/serializers/CustomTokenObtainPairSerializer.py
@@ -0,0 +1,17 @@
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token["username"] = user.username
+        return token
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        user = self.user
+        data["user_id"] = user.id
+        data["username"] = user.username
+        return data
