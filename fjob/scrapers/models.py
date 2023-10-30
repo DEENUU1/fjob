bfa5d0dd414e8d43bb687ea0c2f18e96a23495ad
DEENUU1 <kacperwlodarczy@protonmail.com>
2023-10-30 23:01:01 +0100

    Add PageContent  model to store full html page content

---
+++ fjob/scrapers/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class PageContent(models.Model):
+    content = models.TextField()
+    website = models.CharField(max_length=100)
+    is_parsed = models.BooleanField(default=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.website} - {self.is_parsed} - {self.date_created}"
+
+    class Meta:
+        ordering = ["-date_created"]
+        verbose_name = "Page Content"
+        verbose_name_plural = "Page Contents"
