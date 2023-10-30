337f9e742c8499fd44e7adbd045a9a8fb3214573
DEENUU1 <kacperwlodarczy@protonmail.com>
2023-10-30 17:43:06 +0100

    Add sorting in frontend for Offers

--- frontend/src/Offers/Pages/OffersListPage.jsx
+++ frontend/src/Offers/Pages/OffersListPage.jsx
@@ -1,18 +1,20 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import {OfferList} from "../Components/OfferList.jsx";
+import { OfferList } from "../Components/OfferList.jsx";
 
 export const OffersListPage = () => {
   const [offers, setOffers] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [sortOption, setSortOption] = useState("");
+  const token = localStorage.getItem("access_token");
 
   const loadOffers = () => {
     setIsLoading(true);
 
-    const token = localStorage.getItem("access_token");
+    const sortUrl = sortOption ? `&ordering=${sortOption}` : "";
 
     axios
-      .get(`http://localhost:8000/offers/`, {
+      .get(`http://localhost:8000/offers/?${sortUrl}`, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
@@ -29,7 +31,7 @@ export const OffersListPage = () => {
 
   useEffect(() => {
     loadOffers();
-  }, []);
+  }, [sortOption]);
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -37,8 +39,16 @@ export const OffersListPage = () => {
 
   return (
     <div>
-
-      <OfferList offers={offers}/>
+      <div className="sort-container">
+        <select value={sortOption} onChange={(e) => setSortOption(e.target.value)}>
+          <option value="">Sort by</option>
+          <option value="-date_scraped">Newest</option>
+          <option value="date_scraped">Oldest</option>
+          <option value="-salary__salary_from">Salary highest</option>
+          <option value="salary__salary_from">Salary lowest</option>
+        </select>
+      </div>
+      <OfferList offers={offers} />
     </div>
   );
 };
