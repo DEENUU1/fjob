29f75eeec22f0bc71a42c733024eb77225382d67
DEENUU1 <kacperwlodarczy@protonmail.com>
2023-11-06 14:45:16 +0100

    Add collapse navbar

--- frontend/src/Authentication/Pages/Register.jsx
+++ frontend/src/Authentication/Pages/Register.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useState } from "react";
 import "../Styles/login.css";
-import { useNavigate } from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import { toast } from "react-toastify";
 import {useTranslation} from "react-i18next";
 
@@ -152,7 +152,7 @@ export const Register = () => {
               onChange={() => setTermsAccepted(!termsAccepted)}
               required
             />
-            <label>{t('register.labelterms')}</label>
+            <label><a href="/documents/policy">{t('register.labelterms')} </a> </label>
 
           </div>
 
--- frontend/src/Components/Navigation.jsx
+++ frontend/src/Components/Navigation.jsx
@@ -1,6 +1,6 @@
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-
+import Container from "react-bootstrap/Container";
 import React, { useState, useEffect } from "react";
 import {useTranslation} from "react-i18next";
 import Dropdown from "react-bootstrap/Dropdown";
@@ -24,42 +24,47 @@ export function Navigation() {
 
   return (
     <div>
-      <Navbar bg="light" variant="light">
-        <Navbar.Brand href="/">FJob</Navbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link href="/">{t("nav.home")}</Nav.Link>
-          <Nav.Link href="/contact">{t("nav.contact")}</Nav.Link>
-        </Nav>
-        <Nav>
-          <Dropdown>
-            <Dropdown.Toggle variant="dark" id="dropdown-lang">
-              {t("nav.languages")}
-            </Dropdown.Toggle>
-            <Dropdown.Menu>
-              {Object.keys(locales).map((locale) => (
-                <Dropdown.Item
-                  key={locale}
-                  onClick={() => i18n.changeLanguage(locale)}
-                >
-                  {locales[locale].title}
-                </Dropdown.Item>
-              ))}
-            </Dropdown.Menu>
-          </Dropdown>
-          {isAuth ? (
-            <Nav.Link href="/profile">
-              {localStorage.getItem("username")}
-            </Nav.Link>
-          ) : null}
-          {isAuth ? (
-            <Nav.Link href="/logout">{t("nav.logout")}</Nav.Link>
-          ) : (
-            <Nav.Link href="/login">{t("nav.login")}</Nav.Link>
-          )}
-          {isAuth ? null : (
-            <Nav.Link href="/register">{t("nav.register")}</Nav.Link>
-          )}
-        </Nav>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+          <Navbar.Brand href="/">FJob</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">{t("nav.home")}</Nav.Link>
+            <Nav.Link href="/contact">{t("nav.contact")}</Nav.Link>
+          </Nav>
+          <Nav>
+            <Dropdown>
+              <Dropdown.Toggle variant="dark" id="dropdown-lang">
+                {t("nav.languages")}
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                {Object.keys(locales).map((locale) => (
+                  <Dropdown.Item
+                    key={locale}
+                    onClick={() => i18n.changeLanguage(locale)}
+                  >
+                    {locales[locale].title}
+                  </Dropdown.Item>
+                ))}
+              </Dropdown.Menu>
+            </Dropdown>
+            {isAuth ? (
+              <Nav.Link href="/profile">
+                {localStorage.getItem("username")}
+              </Nav.Link>
+            ) : null}
+            {isAuth ? (
+              <Nav.Link href="/logout">{t("nav.logout")}</Nav.Link>
+            ) : (
+              <Nav.Link href="/login">{t("nav.login")}</Nav.Link>
+            )}
+            {isAuth ? null : (
+              <Nav.Link href="/register">{t("nav.register")}</Nav.Link>
+            )}
+          </Nav>
+         </Navbar.Collapse>
+        </Container>
       </Navbar>
     </div>
   );
